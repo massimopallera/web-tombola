b25b9ef6e2935c219a16345273e2032876ec4ef7
massimopallera <pallera.massimo@gmail.com>
2024-12-22 00:28:25 +0100

    added buttons and extracted number (not dynamic)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const table = document.getElementById('table');
 for (let i = 0; i < 100; i++){
     const markup = `
         <div class="col-1">
-            <div class="card">
+            <div class="card" id=${i}>
                 ${i}
             </div>
         </div>
