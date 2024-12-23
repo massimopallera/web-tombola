e7d714788e0e0c6eb92c852c5216d45386552000
massimopallera <pallera.massimo@gmail.com>
2024-12-23 12:53:38 +0100

    reordered script

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const table = document.getElementById('table');
 
+// Variables
+const endBtn = document.getElementById('endgame');
+const table = document.getElementById('table');
+const extractBtn = document.getElementById('extract');
+const textEl = document.querySelector('#buttons-container > h3')
+const text = 'Ultimo numero estratto'
+
+// Create table numbers
 for (let i = 0; i < 100; i++){
     const markup = `
         <div class="col-1">
@@ -11,31 +18,29 @@ for (let i = 0; i < 100; i++){
 
 
     table.innerHTML += markup;
-}
+};
 
-const extractBtn = document.getElementById('extract');
+// Logic for Extraction Button
 extractBtn.addEventListener('click', () => {
     let number = Math.floor(Math.random()*100)
 
     if (Array.from(document.getElementsByClassName('active')).length === 100){
         alert('Tutti i numeri sono stati estratti!')
-        extractBtn.ariaDisabled
+        extractBtn.disabled = true
         return
     }
-
-
 
     while (document.getElementById(number).classList.contains('active') && Array.from(document.getElementsByClassName('active')).length < 100) {
         number = Math.floor(Math.random()*100)        
     }
 
     document.getElementById('extracted').innerHTML = number
+    textEl.innerHTML == '' ? textEl.innerHTML = text : null
     document.getElementById(number).classList.add('active')
     console.log(document.getElementById(number))
 });
 
-
-const endBtn = document.getElementById('endgame');
+// Reset the table
 endBtn.addEventListener('click', () => {
     Array.from(document.getElementsByClassName('card')).forEach(card => card.classList.remove('active'));
     document.getElementById('extracted').innerHTML = '';
