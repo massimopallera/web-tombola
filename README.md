9efd7ccc279359bb13c2bf7d7dadb20653dbfab0
massimopallera <pallera.massimo@gmail.com>
2024-12-24 02:05:07 +0100

    disabled reset btn until 15 numbers are extracted

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # ToDo:
 - ~~Handle active numbers style~~
-- Handle logic for numbers
+- ~~Handle logic for numbers~~
 - ~~Make logic for buttons~~
-- Make page responsive
+- ~~Make page responsive~~
+- Create form to get the number of cartels to create
+- Make logic for cartels (15 numbers each cartel)
+- Create different cartels, not equals
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 // Text Strings
 const lastExtracted = 'Ultimo numero estratto:'
 const endGame = 'Tutti i numeri estratti!'
+const defaultText = '';
 
 // Variables
 const endBtn = document.getElementById('endgame');
 const table = document.getElementById('table');
 const extractBtn = document.getElementById('extract');
 const lastExtractedEl = document.querySelector('#buttons-container > h3')
-// lastExtractedEl.style.visibilty = 'hidden'
-
 
 
 // Create table numbers
@@ -31,8 +30,6 @@ extractBtn.addEventListener('click', () => {
     let number = Math.floor(Math.random()*100)
 
     const activeEls = document.getElementsByClassName('active')
-    // console.log(activeEls);
-    
 
     if (Array.from(activeEls).length === 100){
         lastExtractedEl.innerHTML = endGame
@@ -46,8 +43,7 @@ extractBtn.addEventListener('click', () => {
     }
 
     document.getElementById('extracted').innerHTML = number
-    // lastExtractedEl.style.visibility == 'hidden' ? lastExtractedEl.style.visibility = 'visible' : null
-    lastExtractedEl.innerHTML == '' ? lastExtractedEl.innerHTML = lastExtracted : null
+    lastExtractedEl.innerHTML == defaultText ? lastExtractedEl.innerHTML = lastExtracted : null
     lastExtractedEl.classList.contains('hide') ? lastExtractedEl.classList.remove('hide') : null
 
     document.getElementById(number).classList.add('active')
@@ -56,8 +52,10 @@ extractBtn.addEventListener('click', () => {
 
 // Reset the table
 endBtn.addEventListener('click', () => {
-    Array.from(document.getElementsByClassName('card')).forEach(card => card.classList.remove('active'));
-    document.getElementById('extracted').innerHTML = '';
-    lastExtractedEl.classList.add('hide')
-    extractBtn.disabled = false
+    if (document.getElementsByClassName('active').length >= 15){
+        Array.from(document.getElementsByClassName('card')).forEach(card => card.classList.remove('active'));
+        document.getElementById('extracted').innerHTML = defaultText;
+        lastExtractedEl.classList.add('hide')
+        extractBtn.disabled = false
+    }
 });
